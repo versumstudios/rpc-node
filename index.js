292ee31fc8a49cdbb4f6d1e275e9d716db53a191
andrevenancio <info@andrevenancio.com>
2022-05-31 02:11:21 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+exports.mainnet = [
+  "mainnet.api.tez.ie",
+  "mainnet.smartpy.io",
+  "rpc.tzbeta.net",
+  "teznode.letzbake.com",
+  "mainnet-tezos.giganode.io",
+  "mainnet.tezos.marigold.dev",
+  "rpc-mainnet.ateza.io",
+  "eu01-node.teztools.net",
+  "rpc.tzkt.io/mainnet",
+]
+
+exports.hangzhounet = [
+  "hangzhounet.api.tez.ie",
+  "hangzhounet.smartpy.io",
+  "testnet-tezos.giganode.io",
+  "rpc-testnet-one.ateza.io",
+  "rpc.tzkt.io/hangzhou2net",
+]
